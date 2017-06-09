8743713c3e6aa8a6cd6209956bb9fa853356ae01
Natasha Osborne <natasha08@me.com>
2017-06-09 14:23:54 -0600

    Initial Commit

---
+++ FitnessApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FitnessApp
+//
+//  Created by Natasha Osborne on 6/9/17.
+//  Copyright © 2017 Natasha Osborne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
