5be6238bea4cbac3680f44860c5b36f36d4fd019
Natasha Osborne <natasha08@me.com>
2017-06-09 17:14:09 -0600

    Project creation. Super basic home/login page.

--- FitnessApp/ViewController.swift
+++ FitnessApp/ViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loginButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBAction func buttonPressed(_ sender: Any) {
+        print("BUTTON PRESSED")
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
